983221f1e2c0a37d69f014c8cba564b5d7e18c9e
Restyled.io <commits@restyled.io>
2023-01-10 20:22:35 +0000

    Restyled by clang-format

--- TarkovDevData/Extensions/TarkovDevDataExtensions.cs
+++ TarkovDevData/Extensions/TarkovDevDataExtensions.cs
@@ -5,16 +5,15 @@ using TarkovDevData.Services.Interfaces;
 
 namespace TarkovDevData.Extensions;
 
-public static class TarkovDevDataExtensions
-{
-    public static void AddTarkovDevData(this IServiceCollection services)
-    {
-        services.AddHttpClient<ITarkovDevDataService, TarkovDevDataService>(client =>
-        {
-            client.BaseAddress = new Uri(Constants.TarkovDevUrl, UriKind.Absolute);
-            client.Timeout = TimeSpan.FromSeconds(30);
-            client.DefaultRequestVersion = HttpVersion.Version20;
-            client.DefaultRequestHeaders.Add("User-Agent", "EFTToolsAPI");
+public static class TarkovDevDataExtensions {
+  public static void AddTarkovDevData(this IServiceCollection services) {
+    services.AddHttpClient<ITarkovDevDataService, TarkovDevDataService>(
+        client => {
+          client.BaseAddress =
+              new Uri(Constants.TarkovDevUrl, UriKind.Absolute);
+          client.Timeout = TimeSpan.FromSeconds(30);
+          client.DefaultRequestVersion = HttpVersion.Version20;
+          client.DefaultRequestHeaders.Add("User-Agent", "EFTToolsAPI");
         });
-    }
+  }
 }
--- TarkovDevData/Services/Interfaces/ITarkovDevDataService.cs
+++ TarkovDevData/Services/Interfaces/ITarkovDevDataService.cs
@@ -3,8 +3,7 @@ using TarkovDevData.Models.Item;
 
 namespace TarkovDevData.Services.Interfaces;
 
-public interface ITarkovDevDataService
-{
-    Task<List<Item>?> GetAllItems(CancellationToken ct);
-    Task<List<Ammo>?> GetAmmo(CancellationToken ct);
+public interface ITarkovDevDataService {
+  Task<List<Item>?> GetAllItems(CancellationToken ct);
+  Task<List<Ammo>?> GetAmmo(CancellationToken ct);
 }
